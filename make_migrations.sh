a8a37703ba6120d45fc7a8544305f4023396b93b
TaZxQ <tajwargoni98@gmail.com>
2021-04-03 13:16:56 +0500

    write migrate and migrattions bash files

---
+++ make_migrations.sh
@@ -0,0 +1,3 @@
+python3 manage.py makemigrations user --settings=miniproject.settings
+python3 manage.py makemigrations item --settings=miniproject.settings
+python3 manage.py makemigrations batch --settings=miniproject.settings
