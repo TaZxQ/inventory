4a2e5d10d7be69597dbfb0de2f269af4643b2654
TaZxQ <tajwargoni98@gmail.com>
2021-04-03 15:49:56 +0500

    implemented batch endpoints

--- miniproject/batch/views.py
+++ miniproject/batch/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+# Local imports
+from .models import Batch
+from .serializers import BatchSerializer
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import viewsets, permissions, status
 
-# Create your views here.
+
+class BatchViewSet(viewsets.ModelViewSet):
+    queryset = Batch.objects.all()
+    serializer_class = BatchSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if not request.user.is_superuser:
+            queryset = Batch.objects.filter(manager=request.user)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        # users other than admin/superuser can not perform this action
+        if not request.user.is_superuser:
+            raise PermissionDenied
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def destroy(self, request, *args, **kwargs):
+        # users other than admin/superuser can not perform this action
+        if not request.user.is_superuser:
+            raise PermissionDenied
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def update(self, request, *args, **kwargs):
+        # users other than admin/superuser can not perform this action
+        if not request.user.is_superuser:
+            raise PermissionDenied
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
