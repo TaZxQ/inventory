115639ac966c2b7007d833fe6f82df59cf34eff4
TaZxQ <tajwargoni98@gmail.com>
2021-04-03 13:29:49 +0500

    user model, manager and migrations

--- miniproject/user/models.py
+++ miniproject/user/models.py
@@ -2,6 +2,7 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from .managers import UserManager
 
 
 class UserAccount(AbstractBaseUser):
@@ -10,18 +11,19 @@ class UserAccount(AbstractBaseUser):
     first_name = models.CharField(max_length=30, unique=False, null=True)
     middle_name = models.CharField(max_length=30, unique=False, null=True)
     last_name = models.CharField(max_length=30, unique=False, null=True)
-    first_and_last_name = models.CharField(max_length=60, unique=False, null=True)
     city = models.CharField(max_length=60, unique=False, null=True)
     state = models.CharField(max_length=60, unique=False, null=True)
-    identifier = models.CharField(max_length=30, unique=False, null=True)
     date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-    last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
 
+    objects = UserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     def __str__(self):
         """Return string representation of our user"""
         return self.email
+
+    class Meta:
+        db_table = 'user_account'
