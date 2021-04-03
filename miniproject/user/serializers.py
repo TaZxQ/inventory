6ad9ebc77a41790db1169c7d9d45d8273461b439
TaZxQ <tajwargoni98@gmail.com>
2021-04-04 00:10:12 +0500

    settings.py

--- miniproject/user/serializers.py
+++ miniproject/user/serializers.py
@@ -1,6 +1,7 @@
 from .models import UserAccount
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
+from sendgrid import Mail, Cc, SendGridAPIClient
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,6 +12,22 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         # get random password here to send email to new user
         random_password = self.Meta.model.objects.make_random_password()
-        print(random_password)
+
+        # sendgrid API integration to send the managers their password on sign up
+
+        message = Mail(
+            from_email=('mian.waqashafiz@gmail.com', 'mini inventory'),
+            to_emails='mian.waqashafiz@gmail.com',
+            subject='Sending with Twilio SendGrid is Fun',
+            html_content=f'<strong>Your password is {random_password}</strong>')
+        try:
+            sg = SendGridAPIClient('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')
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e.message)
+
         validated_data['password'] = make_password(random_password)
         return super(UserSerializer, self).create(validated_data)
